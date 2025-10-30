6d3cc26adfbdde6eb8e5e8945f513a8457df226a
MoreAndMore <abulljk@gmail.com>
2025-10-30 13:34:19 +0500

    email fix

--- internal/auth-service/adapters/service/db/driver_repo.go
+++ internal/auth-service/adapters/service/db/driver_repo.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
-
-	"ride-hail/internal/auth-service/core/domain/models"
-
 	"github.com/jackc/pgx/v5"
+	"ride-hail/internal/auth-service/core/domain/models"
 )
 
 type DriverRepo struct {
@@ -38,8 +36,8 @@ func (dr *DriverRepo) Create(ctx context.Context, driver models.Driver) (string,
 
 	// Fixed query to insert driver with correct columns
 	q := `INSERT INTO drivers (
-		username, email, password_hash, license_number, vehicle_type, vehicle_attrs
-	) VALUES ($1, $2, $3, $4, $5, $6) RETURNING driver_id`
+      username, email, password_hash, license_number, vehicle_type, vehicle_attrs
+   ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING driver_id`
 
 	id := ""
 	var vehicleAttrs interface{}
@@ -57,6 +55,7 @@ func (dr *DriverRepo) Create(ctx context.Context, driver models.Driver) (string,
 		driver.VehicleType,
 		vehicleAttrs,
 	)
+
 	if err = row.Scan(&id); err != nil {
 		return "", fmt.Errorf("failed to insert driver: %w", err)
 	}
@@ -71,27 +70,26 @@ func (dr *DriverRepo) Create(ctx context.Context, driver models.Driver) (string,
 
 func (dr *DriverRepo) GetByEmail(ctx context.Context, email string) (models.Driver, error) {
 	q := `
-		SELECT 
-			driver_id,
-			created_at,
-			updated_at,
-			username,
-			email,
-			password_hash,
-			coord,
-			license_number,
-			vehicle_type,
-			vehicle_attrs,
-			rating,
-			total_rides,
-			total_earnings,
-			status,
-			is_verified
-		FROM 
-			drivers
-		WHERE
-			email = $1
-	`
+      SELECT
+         driver_id,
+         created_at,
+         updated_at,
+         username,
+         email,
+         password_hash,
+         license_number,
+         vehicle_type,
+         vehicle_attrs,
+         rating,
+         total_rides,
+         total_earnings,
+         status,
+         is_verified
+      FROM
+         drivers
+      WHERE
+         email = $1
+   `
 
 	var d models.Driver
 	err := dr.db.conn.QueryRow(ctx, q, email).Scan(
@@ -101,7 +99,6 @@ func (dr *DriverRepo) GetByEmail(ctx context.Context, email string) (models.Driv
 		&d.Username,
 		&d.Email,
 		&d.PasswordHash,
-		&d.Coord,
 		&d.LicenseNumber,
 		&d.VehicleType,
 		&d.VehicleAttrs,
@@ -111,6 +108,7 @@ func (dr *DriverRepo) GetByEmail(ctx context.Context, email string) (models.Driv
 		&d.Status,
 		&d.IsVerified,
 	)
+
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return models.Driver{}, ErrUnknownEmail
